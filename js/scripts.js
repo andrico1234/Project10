5316201da2151f0d28b08e8406450085f4f72da3
andrico1234 <andrico1234@yahoo.co.uk>
2016-06-28 15:31:07 +0100

    gh-pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,6 @@
 /**
  * Created by Home on 16/06/2016.
  */
-// Ajax Requests
-
-
-var pokemonArrayOverlay = [];
-// put the ids into an array
-// this array will then be cycled through using a for loop to create our initial pokemon database.
-
-// PokemonAPI
-
-// var pokemonIdArray = [1, 4, 7, 50, 74, 112, 141, 149];
-// $.each(pokemonIdArray, function (i, id) {
-//     var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + id + "/";
-//     var pokemonGalleryItem = "<ul>";
-//     $.getJSON(pokemonURL, function (pokemonData) {
-//         var pokemonDataOverlay = [pokemonData.name, pokemonData.id];
-//         pokemonArrayOverlay.push(pokemonDataOverlay);
-//         pokemonGalleryItem = "<li><div class='gallery-image'>";
-//         pokemonGalleryItem += "<img src='" + pokemonData.sprites.front_default + "' alt=''>";
-//         pokemonGalleryItem += "</div></li>";
-//         // console.log(pokemonArrayOverlay[i][0], pokemonArrayOverlay[i][1]);
-//         if (pokemonData["types"][1]) {
-//             console.log(pokemonData["types"][1]["type"]["name"]);
-//         }
-//         console.log(pokemonData["types"][0]["type"]["name"]);
-//         pokemonGalleryItem += "</ul>";
-//         $(".ajax-gallery").append(pokemonGalleryItem);
-//     });
-// });
-
-// look in to tiny sort. Gallery items may need to be placed in to a list.
-// create a lightbox. In this lightbox, we'll need a picture of the pokemon, it's name, ID and type(s).
 
 // Create an overlay
 var $overlay = $("<div id='overlay'></div>");
@@ -49,6 +18,59 @@ $overlayImage.attr("src", "img/1.png");
 $pokemonName.text("Bulbasaur");
 $pokemonId.text("#1");
 $pokemonTypes.text("1. Grass 2. Poison");
+$overlay.hide();
+
+function closeArrows() {
+    $("#left-arrow, #right-arrow").hide();
+};
+// Ajax Requests
+
+
+var pokemonArrayOverlay = [];
+// put the ids into an array
+// this array will then be cycled through using a for loop to create our initial pokemon database.
+
+// PokemonAPI
+
+var pokemonIdArray = [1, 4, 7, 50, 74, 112, 141, 149];
+$.each(pokemonIdArray, function (i, id) {
+    var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + id + "/";
+    var pokemonGalleryItem = "<ul>";
+    $.getJSON(pokemonURL, function (pokemonData) {
+        var pokemonDataOverlay = [pokemonData.name, pokemonData.id];
+        pokemonArrayOverlay.push(pokemonDataOverlay);
+        pokemonGalleryItem = "<li><div class='gallery-image'>";
+        pokemonGalleryItem += "<img src='" + pokemonData.sprites.front_default + "' alt=''>";
+        pokemonGalleryItem += "</div></li>";
+        // console.log(pokemonArrayOverlay[i][0], pokemonArrayOverlay[i][1]);
+        if (pokemonData["types"][1]) {
+            console.log(pokemonData["types"][1]["type"]["name"]);
+        }
+        console.log(pokemonData["types"][0]["type"]["name"]);
+        pokemonGalleryItem += "</ul>";
+        $(".ajax-gallery").append(pokemonGalleryItem);
+    });
+});
+
+var starWarsArrayOverlay = [];
+
+// Star Wars API
+
+var starWarsIdArray = [1, 2, 3, 4, 5, 6];
+$.each(starWarsIdArray, function (i, id) {
+    var starWarsURL = "http://swapi.co/api/people/" + id + "/";
+    var starWarsGalleryItem = "<ul>";
+    $.getJSON(starWarsURL, function(starWarsData) {
+        var starWarsDataOverlay = [starWarsData.name, starWarsData.gender];
+    })
+})
+
+
+// look in to tiny sort. Gallery items may need to be placed in to a list.
+// create a lightbox. In this lightbox, we'll need a picture of the pokemon, it's name, ID and type(s).
+
+
+
 // CSS for div boxes will be centered,
 // Some pokemon only have one type, form a conditional statement that appropriately displays this.
 // Clicking the overlay will close the overlay.
