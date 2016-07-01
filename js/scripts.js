c4244a4cf59a41e6faceadedfee98ab4461fa3a9
andrico1234 <andrico1234@yahoo.co.uk>
2016-07-01 16:20:46 +0100

    v0.6.1
    Trouble clicking jQuery images

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,7 @@
 /**
  * Created by Home on 16/06/2016.
  */
-
-// Create an overlay
 var $overlay = $("<div id='overlay'></div>");
-// Inside the overlay will be a container
-// Inside the container will be the image, which the pokemon's name, ID and type(s)
 var $overlayImage = $("<img id='overlayImage' src='' alt=''/>");
 var $pokemonName = $("<p id='pokemonName'></p>");
 var $pokemonId = $("<p id='pokemonId'></p>");
@@ -13,12 +9,14 @@ var $pokemonTypes = $("<p id='pokemonTypes'></p>");
 
 $overlay.append($overlayImage).append($pokemonName).append($pokemonId).append($pokemonTypes);
 $("body").append($overlay);
-
 $overlayImage.attr("src", "img/1.png");
 $pokemonName.text("Bulbasaur");
 $pokemonId.text("#1");
 $pokemonTypes.text("1. Grass 2. Poison");
 $overlay.hide();
+
+var pokemonArrayOverlay = [];
+var starWarsArrayOverlay = [];
 
 function closeArrows() {
     $("#left-arrow, #right-arrow").hide();
@@ -30,24 +28,15 @@ function showOverlay() {
     // clicking an image will bring up the overlay and the arrows.
 }
 
-$(".gallery-image").click(function () {
-    console.log("image click");
-    showOverlay();
-});
-
-//
-// $('body').click(function () {
-//     if ($overlay.show) {
-//         $overlay.hide();
-//         closeArrows();
-//     }
-// });
+function closeOverlay() {
+    closeArrows();
+    $overlay.hide();
+}
 
 closeArrows();
 // Ajax Requests
 
 
-var pokemonArrayOverlay = [];
 // put the ids into an array
 // this array will then be cycled through using a for loop to create our initial pokemon database.
 
@@ -56,50 +45,49 @@ var pokemonArrayOverlay = [];
 var pokemonIdArray = [1, 4, 7, 50, 74, 112, 141, 149];
 $.each(pokemonIdArray, function (i, id) {
     var pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + id + "/";
-    var pokemonGalleryItem = "<ul>";
     $.getJSON(pokemonURL, function (pokemonData) {
         var pokemonDataOverlay = [pokemonData.name, pokemonData.id];
         pokemonArrayOverlay.push(pokemonDataOverlay);
-        pokemonGalleryItem = "<li>";
-        pokemonGalleryItem += "<img class='gallery-image' src='" + pokemonData.sprites.front_default + "' alt=''/>";
-        pokemonGalleryItem += "</li>";
+        var $pokemonGalleryItem = "<li>";
+        $pokemonGalleryItem += "<img class='gallery-image' alt='n/a' src='" + pokemonData.sprites.front_default + "'/>";
+        $pokemonGalleryItem += "</li>";
         // console.log(pokemonArrayOverlay[i][0], pokemonArrayOverlay[i][1]);
         if (pokemonData["types"][1]) {
             console.log(pokemonData["types"][1]["type"]["name"]);
         }
         console.log(pokemonData["types"][0]["type"]["name"]);
-        pokemonGalleryItem += "</ul>";
-        $(".ajax-gallery").append(pokemonGalleryItem);
+        $(".ajax-gallery").append($pokemonGalleryItem);
     });
 });
 
-var starWarsArrayOverlay = [];
 
 // Star Wars API
 var starWarsIdArray = [1, 2, 3, 4, 5, 6, 7];
 $.each(starWarsIdArray, function (i, id) {
     var starWarsURL = "https://swapi.co/api/people/" + id + "/";
-    var starWarsGalleryItem = "<ul>";
     $.getJSON(starWarsURL, function (starWarsData) {
         var starWarsDataOverlay = [starWarsData.name, starWarsData.gender];
         starWarsArrayOverlay.push(starWarsDataOverlay);
-        starWarsGalleryItem = "<li>";
-        starWarsGalleryItem += "<img class='gallery-image' src='img/starwars"+ id +".jpg' alt=''/>";
+        var starWarsGalleryItem = "<li>";
+        starWarsGalleryItem += "<img class='gallery-image' alt='n/a' src='img/starwars" + id + ".jpg' alt=''/>";
         starWarsGalleryItem += "</li>";
         console.log(starWarsData.name, starWarsData.gender);
-        starWarsGalleryItem += "</ul>";
         $(".starwars-gallery").append(starWarsGalleryItem);
     });
 });
-
-
 // look in to tiny sort. Gallery items may need to be placed in to a list.
 // create a lightbox. In this lightbox, we'll need a picture of the pokemon, it's name, ID and type(s).
 
 
-// CSS for div boxes will be centered,
 // Some pokemon only have one type, form a conditional statement that appropriately displays this.
-// Clicking the overlay will close the overlay.
+$overlay.click(function() {
+    closeOverlay();
+});
+
+$(".ajax-gallery .gallery-image").click(function() {
+    console.log("image clicked");
+    showOverlay();
+});
 
 // store each pokemon's data into an array or object?
 // Define an empty array, and push that pokemon's data to the array when during the $.each() function.
