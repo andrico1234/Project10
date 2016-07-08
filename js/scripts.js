3ca5a6558044b6747a99612d04c4105f2234a8f9
andrico1234 <andrico1234@yahoo.co.uk>
2016-07-08 12:49:11 +0100

    v0.8
    - Need to add sort fnct.
    - Validate

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,40 @@
 /**
  * Created by Home on 16/06/2016.
  */
-var $overlay = $("<div id='overlay'></div>");
-var $overlayImage = $("<img id='overlayImage' src='' alt=''/>");
-var $itemName = $("<p id='itemName'></p>");
-var $itemID = $("<p id='itemID'></p>");
-var $itemTypes = $("<p id='itemTypes'></p>");
-var $itemGender = $("<p id='itemGender'></p>");
-
-$overlay.append($overlayImage).append($itemName).append($itemID).append($itemTypes).append($itemGender);
-$("body").append($overlay);
+// var $overlay = $("<div id='overlay'></div>");
+// var $overlayImage = $("<img id='overlayImage' src='' alt=''/>");
+// var $itemName = $("<p id='itemName'></p>");
+// var $itemID = $("<p id='itemID'></p>");
+// var $itemTypes = $("<p id='itemTypes'></p>");
+// var $itemGender = $("<p id='itemGender'></p>");
+//
+// $overlay.append($overlayImage).append($itemName).append($itemID).append($itemTypes).append($itemGender);
+// $("body").append($overlay);
 // $overlayImage.attr("src", "img/1.png");
 // $itemName.text("Bulbasaur");
 // $itemID.text("#1");
 // $itemTypes.text("1. Grass 2. Poison");
-$overlay.hide();
+// $overlay.hide();
 
 var pokemonArrayOverlay = [];
 var starWarsArrayOverlay = [];
 
-function closeArrows() {
-    $("#left-arrow, #right-arrow").hide();
-}
-
-function showOverlay() {
-    $("#left-arrow, #right-arrow").show();
-    $overlay.show();
-    // clicking an image will bring up the overlay and the arrows.
-}
-
-function closeOverlay() {
-    closeArrows();
-    $overlay.hide();
-}
-
-closeArrows();
+// function closeArrows() {
+//     $("#left-arrow, #right-arrow").hide();
+// }
+//
+// function showOverlay() {
+//     $("#left-arrow, #right-arrow").show();
+//     $overlay.show();
+//     // clicking an image will bring up the overlay and the arrows.
+// }
+//
+// function closeOverlay() {
+//     closeArrows();
+//     $overlay.hide();
+// }
+//
+// closeArrows();
 // Ajax Requests
 
 
@@ -49,13 +49,13 @@ $.each(pokemonIdArray, function (i, id) {
     $.getJSON(pokemonURL, function (pokemonData) {
         var pokemonDataOverlay = [pokemonData.name, pokemonData.id];
         pokemonArrayOverlay.push(pokemonDataOverlay);
-        var $pokemonGalleryItem = "<li>";
-        $pokemonGalleryItem += "<img class='gallery-image' alt='n/a' src='" + pokemonData.sprites.front_default + "'/>";
+        var pokemonType = pokemonData["types"][0]["type"]["name"];
+        var $pokemonGalleryItem = "<li class='poke'>";
+        $pokemonGalleryItem += "<a data-lightbox='image-pokemon' data-title='" + pokemonData.name + "</br>" +
+            pokemonData.id + "</br>" + pokemonType + "' href='" + pokemonData.sprites.front_default + "'>";
+        $pokemonGalleryItem += "<img class='gallery-image' alt='n/a' src='" + pokemonData.sprites.front_default + "'/></a>";
         $pokemonGalleryItem += "</li>";
-        if (pokemonData["types"][1]) {
-            console.log(pokemonData["types"][1]["type"]["name"]);
-        }
-        console.log(pokemonData["types"][0]["type"]["name"]);
+
         $(".ajax-gallery").append($pokemonGalleryItem);
     });
 });
@@ -68,10 +68,14 @@ $.each(starWarsIdArray, function (i, id) {
     $.getJSON(starWarsURL, function (starWarsData) {
         var starWarsDataOverlay = [starWarsData.name, starWarsData.gender];
         starWarsArrayOverlay.push(starWarsDataOverlay);
-        var starWarsGalleryItem = "<li>";
+        var starWarsGalleryItem = "<li class='star'>";
+
+        starWarsGalleryItem += "<a data-lightbox='image-starwars' data-title='" + starWarsData.name + "</br>" +
+            starWarsData.gender + "' href='img/starwars" + id + ".jpg'>";
+
         starWarsGalleryItem += "<img class='gallery-image' alt='n/a' src='img/starwars" + id + ".jpg' alt=''/>";
         starWarsGalleryItem += "</li>";
-        console.log(starWarsData.name, starWarsData.gender);
+        // console.log(starWarsData.name, starWarsData.gender);
         $(".starwars-gallery").append(starWarsGalleryItem);
     });
 });
@@ -80,21 +84,30 @@ $.each(starWarsIdArray, function (i, id) {
 
 
 // Some pokemon only have one type, form a conditional statement that appropriately displays this.
-$overlay.click(function() {
-    closeOverlay();
-});
+// $overlay.click(function () {
+//     closeOverlay();
+// });
 
-$("body").on('click', '.gallery-image', function() {
-    console.log("image clicked");
-    showOverlay();
-    var imageUrl = $(this).attr('src'); // This is the link to the image.
-    $overlayImage.attr('src', imageUrl);
-    $itemName.text(itemName);
-    $itemID.text(itemID);
-    $itemTypes.text();
-    $itemGender.text();
-    console.log($(this).attr('src'));
-});
+
+// $("body").on('click', '.gallery-image', function () {
+//     showOverlay();
+//     var imageUrl = $(this).attr('src'); // This is the link to the image.
+//     $overlayImage.attr('src', imageUrl);
+//     var pParent = document.getElementsByClassName("poke");
+//     console.log( pParent.contains );
+//     var sParent = document.querySelector(".starwars-gallery");
+//     if ($(this).parent === "li.poke") {
+//         $itemName.text(itemName);
+//         $itemID.text(itemID);
+//         $itemTypes.text();
+//         console.log("hi!!");
+//     } else {
+//         $itemName.text(itemName);
+//         $itemID.text(itemID);
+//         $itemGender.text();
+//         console.log("bye!!");
+//     }
+// });
 
 // store each pokemon's data into an array or object?
 // Define an empty array, and push that pokemon's data to the array when during the $.each() function.
@@ -103,8 +116,8 @@ $("body").on('click', '.gallery-image', function() {
 
 
 // API provide more than 6 items (Done)
-// Pop-ups contain item details. (Working On)
-// Navigate between popups
+// Pop-ups contain item details. (Done)
+// Navigate between popups (Done)
 // Items can be sorted
 // More than 1 api (Done)
 // Validate.
